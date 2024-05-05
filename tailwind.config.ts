a990f3bdbd9e6d949c1c943d35a78aa3eb062250
Mohmd <mim.mohrechi@gmail.com>
2024-05-05 23:52:21 +0330

    added colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                background: "#F0F2FF",
+                background2: "#E5EAFF",
+                primary: "#070D2F",
+                lightBorder: "#070D2F",
+                dbackground: "#050A1E",
+                dbackground2: "#060813",
+                dprimary: "#E5EAFF",
+                dlightBorder: "#080E2B",
 
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
