d257d38108243c25fb9bda33adaf17f41e4e85d4
Mohmd <mim.mohrechi@gmail.com>
2024-05-13 16:28:45 +0330

    minor fix

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,15 +1,15 @@
 import { t } from "i18next";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //NotFound component
 const NotFound = () => {
     return (
         <div className="flex flex-col justify-center items-center pb-48 grow bg-background text-primary dark:text-dprimary">
-            <p className="relative text-8xl font-black">404</p>
+            <p className="text-8xl font-black">404</p>
             <p className="text-4xl">{t("common.pageNotFound")}</p>
-            <NavLink to="/" className="mt-2 text-xl">
+            <Link to="/" className="mt-2 text-xl">
                 {t("common.pageNotFoundLink")}
-            </NavLink>
+            </Link>
         </div>
     );
 };
