9da64719d3aa4862275e27dff40a0144b5840a6e
Mohmd <mim.mohrechi@gmail.com>
2024-05-14 00:17:49 +0330

    outside click detector hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useOutsideClick = (callback: () => void): React.RefObject<HTMLDivElement> => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('click', handleClick, true);
+
+    return () => {
+      document.removeEventListener('click', handleClick, true);
+    };
+  }, [ref, callback]);
+
+  return ref;
+};
+
+export default useOutsideClick;
