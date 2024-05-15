3a4ce8f7ef828e2a300580447afc970d32f27708
Mohmd <mim.mohrechi@gmail.com>
2024-05-15 16:35:26 +0330

    list users assigned to project

--- src/components/Projects/ProjectUsers.tsx
+++ src/components/Projects/ProjectUsers.tsx
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
 import Loader from "../ui/Loader";
 import ErrorPage from "../ui/ErrorPage";
 import { customError } from "src/types/customError";
 import { apiGetUserProjects } from "src/api/user-projects/apiGetUserProjects";
 import { userType } from "src/types/auth";
+import { useQuery } from "@tanstack/react-query";
 
 //ProjectUsers component
 type ProjectUsersProps = { id: number };
@@ -13,13 +13,23 @@ const ProjectUsers = ({ id }: ProjectUsersProps) => {
         queryFn: () =>
             apiGetUserProjects({ project: id }).then((res) => res.data[0].data), //todo param?
     });
-    if (isLoading) <Loader />;
+    if (isLoading) <Loader size={40} />;
     if (error) <ErrorPage error={error as customError} />;
     return (
-        <div className="p-5 rounded-xl border text-primary bg-background border-lightBorder dark:text-dprimary dark:bg-dbackground dark:border-dlightBorder">
-            {data.map((item:{user:userType}) => (
-                <p>{item.user.name}</p>
-            ))}
+        <div className="px-5 rounded-xl border text-primary bg-background border-lightBorder dark:text-dprimary dark:bg-dbackground dark:border-dlightBorder">
+            {data &&
+                data.map((item: { id: number; user: userType }) => (
+                    <p
+                        className="relative top-[1px] py-2 border-b border-lightBorder dark:border-dlightBorder"
+                        key={item.id}
+                    >
+                                    {item.user.name}
+                                    <span className="px-2 text-lightBorder dark:text-dlightBorder">
+                                        |
+                                    </span>
+                                    {item.user.email}
+                    </p>
+                ))}
         </div>
     );
 };
