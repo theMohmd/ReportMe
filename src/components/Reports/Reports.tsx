e8d7c677c31824a2f3bcef0cb2100cb98a8ddd18
Mohmd <mim.mohrechi@gmail.com>
2024-05-16 11:35:03 +0330

    minor fix

--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 import { customError } from "types/customError";
+import { apiGetUserProjects } from "api/user-projects/apiGetUserProjects";
 
 import Loader from "components/ui/Loader";
 import ErrorPage from "components/ui/ErrorPage";
 import ReportsUi from "./ReportsUi";
-import { apiGetUserProjects } from "src/api/user-projects/apiGetUserProjects";
 
 //all project to report
 const Reports = () => {
@@ -20,7 +20,6 @@ const Reports = () => {
             <Loader size={100} className="text-primary dark:text-dprimary" />
         );
     if (error) return <ErrorPage error={error as customError} />; //todo
-        console.log(data)
     return (
         data && (
             <ReportsUi
