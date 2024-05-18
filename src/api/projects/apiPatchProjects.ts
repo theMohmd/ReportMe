7e64c5264dc821cd9b69dceff0146f16ade087a6
Mohmd <mim.mohrechi@gmail.com>
2024-05-19 02:58:41 +0330

    add patch request for projects

---
+++ src/api/projects/apiPatchProjects.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { projectType } from "types/projectType";
+import { getCookie } from "utils/cookie";
+
+//post projects
+export type apiPatchProjectsInputType = { id: number; title: string, description: string; file: File | ""; }
+export type apiPatchProjectsOutputType = projectType
+
+export const apiPatchProjects
+: ( input: apiPatchProjectsInputType ) => Promise<apiPatchProjectsOutputType>
+= async ( input ) => {
+    const {id, ...rest} = input
+    return axios
+    .post(`http://127.0.0.1:8000/api/projects/${id}`, rest, {
+        params:{_method:"put"},
+        headers:{
+            "Content-Type": "multipart/form-data",
+            Accept: "application/json",
+            Authorization: "Bearer " + getCookie("token"),
+        },
+    })
+    .then(res=>res.data.data)
+}
+
