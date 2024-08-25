a289cc543a4627ea7cc3e1d822b32b867dd19d09
Mohmd <mim.mohrechi@gmail.com>
2024-08-25 20:04:52 +0330

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            src: "/src",
-            api: "/src/api",
-            components: "/src/components",
-            contexts: "/src/contexts",
-            hooks: "/src/hooks",
-            types: "/src/types",
-            utils: "/src/utils",
-        },
+  base: "/reportme/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      api: "/src/api",
+      components: "/src/components",
+      contexts: "/src/contexts",
+      hooks: "/src/hooks",
+      types: "/src/types",
+      utils: "/src/utils",
     },
+  },
 });
